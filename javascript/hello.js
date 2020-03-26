f7155abd78f7a2a2002a366f25377c2fa0b15683
Marco Hilpert <marcohilpert@Marcos-Air.home>
2020-03-26 16:12:01 +0100

    Javascript einstieg

---
+++ javascript/hello.js
@@ -0,0 +1,17 @@
+// alert("Hello world");
+
+//let message = "Hello World";
+//alert(message);
+//message = "Hello World";
+//alert(message);
+
+const  LINK_COLOR = "'ff0000";
+console.log("link bitte in der Farbe", LINK_COLOR);
+
+let highscore = 520233;
+console.log(highscore / 10);
+
+let firstname = "John";
+let lastname = "Smith";
+
+let fullname = "Jeffrey Lebowski";
