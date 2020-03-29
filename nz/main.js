4795fa7b81b534280b50913cafec14b8251b14b3
Marco Hilpert <marcohilpert@Marcos-Air.home>
2020-03-29 14:45:15 +0200

    variablen erstellen, query eingefügt

--- nz/main.js
+++ nz/main.js
@@ -1,4 +1,8 @@
-var map = L.map('map').setView[170.477121,-44.004674];
+
+
+let map = document.querySelector("#map")
+
+var map = L.map('map').setView([-44.004674,170.477121], 13) ;
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -7,4 +11,8 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'your.mapbox.access.token'
-}).addTo(map);
+}).addTo(mymap);
+
+var marker = L.marker([[-44.004674,170.477121]]).addTo(mymap);
+
+marker.bindPopup("<b>Lake Tekapo</b><br>a Lake Takapeo Neuseeland").openPopup();
