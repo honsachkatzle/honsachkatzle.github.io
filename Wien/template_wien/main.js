6f0e8bdef04f1e4d6e29267476f51f7e6154a3bd
Marco Hilpert <marcohilpert@Marcos-Air.home>
2020-04-22 14:24:26 +0200

    data feature group erstellen fit bounds, get bounds

--- Wien/template_wien/main.js
+++ Wien/template_wien/main.js
@@ -1,7 +1,7 @@
 let startLayer = L.tileLayer.provider("BasemapAT.grau");
 
 let map = L.map("map", {
-    center: [48.208333, 16.373056],
+    center:[48.208333, 16.37056],
     zoom: 12,
     layers: [
         startLayer
