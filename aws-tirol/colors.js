8a94afc8e30b7f5650ca7aac9827184870c677fb
Marco Hilpert <marcohilpert@Marcos-Air.home>
2020-04-23 17:25:13 +0200

    scheiße mit mc

---
+++ aws-tirol/colors.js
@@ -0,0 +1,88 @@
+Temperatur
+    <rect class = "legend-rect"
+
+width = "41"
+height = "18"
+x = "0"
+y = "0"
+fill = "rgb(250,55,150)" > 
+
+fill = "rgb(255,85,54)" >
+fill = "rgb(255,190,130)" > 
+y = "108"
+fill = "rgb(176,255,188)" > 
+y = "144"
+fill = "rgb(3,205,255)" > 
+fill = "rgb(3,91,190)" > < /rect><rect class="legend-rect" width="41" height="18" x="0" y="198" fill="rgb(120,75,255)"></rect > < rect class = "legend-rect"
+width = "41"
+height = "18"
+x = "0"
+y = "216"
+fill = "rgb(159,128,255)" >
+y2 = "234" > < /line><text class="legend-text" x="20.5" y="18" dy="0.35em" font-size="12" text-anchor="middle">30</text > < text class = "legend-text"
+x = "20.5"
+y = "36"
+dy = "0.35em"
+font - size = "12"
+text - anchor = "middle" > 25 < /text><text class="legend-text" x="20.5" y="54" dy="0.35em" font-size="12" text-anchor="middle">20</text > < text class = "legend-text"
+x = "20.5"
+y = "72"
+dy = "0.35em"
+font - size = "12"
+text - anchor = "middle" > 15 < /text><text class="legend-text" x="20.5" y="90" dy="0.35em" font-size="12" text-anchor="middle">10</text > < text class = "legend-text"
+x = "20.5"
+y = "108"
+dy = "0.35em"
+font - size = "12"
+text - anchor = "middle" > 5 < /text><text class="legend-text" x="20.5" y="126" dy="0.35em" font-size="12" text-anchor="middle">0</text > < text class = "legend-text"
+x = "20.5"
+y = "144"
+dy = "0.35em"
+font - size = "12"
+text - anchor = "middle" > -5 < /text><text class="legend-text" x="20.5" y="162" dy="0.35em" font-size="12" text-anchor="middle">-10</text > < text class = "legend-text"
+x = "20.5"
+y = "180"
+dy = "0.35em"
+font - size = "12"
+text - anchor = "middle" > -15 < /text><text class="legend-text" x="20.5" y="198" dy="0.35em" font-size="12" text-anchor="middle">-20</text > < text class = "legend-text"
+x = "20.5"
+y = "216"
+dy = "0.35em"
+font - size = "12"
+text - anchor = "middle" > -25 < /text>
+
+
+
+
+Wind
+
+<
+    rect class = "legend-rect"
+width = "41"
+height = "18"
+x = "0"
+y = "0"
+fill = "rgb(255,50,50)" >
+y = "36"
+fill = "rgb(100,150,255)" > 
+fill = "rgb(150,255,150)" > 
+x = "0"
+y = "108"
+fill = "rgb(255,255,100)" > 
+
+y2 = "126" > < /line><text class="legend-text" x="20.5" y="18" dy="0.35em" font-size="12" text-anchor="middle">80</text > < text class = "legend-text"
+x = "20.5"
+y = "36"
+dy = "0.35em"
+font - size = "12"
+text - anchor = "middle" > 60 < /text><text class="legend-text" x="20.5" y="54" dy="0.35em" font-size="12" text-anchor="middle">40</text > < text class = "legend-text"
+x = "20.5"
+y = "72"
+dy = "0.35em"
+font - size = "12"
+text - anchor = "middle" > 20 < /text><text class="legend-text" x="20.5" y="90" dy="0.35em" font-size="12" text-anchor="middle">10</text > < text class = "legend-text"
+x = "20.5"
+y = "108"
+dy = "0.35em"
+font - size = "12"
+text - anchor = "middle" > 5 < /text>
