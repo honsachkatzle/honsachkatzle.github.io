fcfaca9b6b9f9014904dbd978cdac7938c370e66
Marco Hilpert <marcohilpert@Marcos-Air.home>
2020-04-30 16:52:35 +0200

    Icon einbauen und positionieren

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -8,26 +8,26 @@ let map = L.map("map", {
     ]
 });
 
-let overlay ={
+let overlay = {
     adlerblicke: L.featureGroup()
 };
 
 L.control.layers({
-    "BasemapAT.grau": L.tileLayer.provider("BasemapAT.grau"),
-    "BasemapAT": L.tileLayer.provider("BasemapAT"),
-    "BasemapAT.highdpi": L.tileLayer.provider("BasemapAT.highdpi"),
-    "BasemapAT.terrain": startLayer,
-    "BasemapAT.surface": L.tileLayer.provider("BasemapAT.surface"),
-    "BasemapAT.orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
-    "BasemapAT.overlay": L.tileLayer.provider("BasemapAT.overlay"),
-    "BasemapAT.orthofoto+overlay": L.layerGroup([
-        L.tileLayer.provider("BasemapAT.orthofoto"),
-        L.tileLayer.provider("BasemapAT.overlay")
-    ])},
-    {
+        "BasemapAT.grau": L.tileLayer.provider("BasemapAT.grau"),
+        "BasemapAT": L.tileLayer.provider("BasemapAT"),
+        "BasemapAT.highdpi": L.tileLayer.provider("BasemapAT.highdpi"),
+        "BasemapAT.terrain": startLayer,
+        "BasemapAT.surface": L.tileLayer.provider("BasemapAT.surface"),
+        "BasemapAT.orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
+        "BasemapAT.overlay": L.tileLayer.provider("BasemapAT.overlay"),
+        "BasemapAT.orthofoto+overlay": L.layerGroup([
+            L.tileLayer.provider("BasemapAT.orthofoto"),
+            L.tileLayer.provider("BasemapAT.overlay")
+        ])
+    }, {
         "Adlerblicke": overlay.adlerblicke
     }
-    
+
 ).addTo(map);
 
 //console.log(ETAPPEN)
@@ -37,13 +37,15 @@ L.control.layers({
 
 for (const blick of ADLERBLICKE) {
     console.log(blick);
-    let mrk = L.marker([blick.lat,blick.lng]).addTo(overlay.adlerblicke);
+    let mrk = L.marker([blick.lat, blick.lng], {
+        icon: L.icon({
+            iconSize: [32, 37],
+            iconAnchor: [16, 37],
+            popupAnchor: [0, -37],
+            iconUrl: "icons/panoramicview.png"
+        })
+    }).addTo(overlay.adlerblicke);
+    L.marker([blick.lat, blick.lng]).addTo(map);
     mrk.bindPopup(`Standort ${blick.standort} (${blick.seehoehe}m)`);
-
-} 
-overlay.adlerblicke.addTo(map); 
-
-
-
-
-
+}
+overlay.adlerblicke.addTo(map);
