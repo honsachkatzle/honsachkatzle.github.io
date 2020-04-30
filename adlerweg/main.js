422989648e8ef3f206b1065513f0a84c79459142
Marco Hilpert <marcohilpert@Marcos-Air.home>
2020-04-30 16:48:50 +0200

    Blicke Marker visualisieren

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -8,6 +8,10 @@ let map = L.map("map", {
     ]
 });
 
+let overlay ={
+    adlerblicke: L.featureGroup()
+};
+
 L.control.layers({
     "BasemapAT.grau": L.tileLayer.provider("BasemapAT.grau"),
     "BasemapAT": L.tileLayer.provider("BasemapAT"),
@@ -19,5 +23,24 @@ L.control.layers({
     "BasemapAT.orthofoto+overlay": L.layerGroup([
         L.tileLayer.provider("BasemapAT.orthofoto"),
         L.tileLayer.provider("BasemapAT.overlay")
-    ])
-}).addTo(map);
+    ])},
+    {
+        "Adlerblicke": overlay.adlerblicke
+    }
+    
+).addTo(map);
+
+//console.log(ETAPPEN)
+//console.log(ADLERBLICKE);
+
+
+
+for (const blick of ADLERBLICKE) {
+    console.log(blick);
+    let mrk = L.marker([blick.lat,blick.lng]).addTo(map);
+    mrk.bindPopup(`Standort ${blick.standort} (${blick.seehoehe}m)`);
+} 
+
+
+
+
