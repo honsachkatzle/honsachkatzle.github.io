d0b78a89bc6fec651738f5c3ee8089945eed798d
Marco Hilpert <marcohilpert@Marcos-MacBook-Air.local>
2020-05-05 14:50:30 +0200

    letzter stand vor hü

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -29,26 +29,28 @@ L.control.layers({
     "Adlerblicke": overlay.adlerblicke,
     "Adlerweg Etappen": overlay.etappen
 }).addTo(map);
+
 //console.log(ETAPPEN);
 //console.log(ADLERBLICKE);
 
 for (const blick of ADLERBLICKE) {
     //console.log(blick);
     let mrk = L.marker([blick.lat, blick.lng], {
-        icon: L.icon({                                       // icon einbauen in script
+        icon: L.icon({
             iconSize: [32, 37],
             iconAnchor: [16, 37],
             popupAnchor: [0, -37],
-            iconUrl: "icons/panoramicview.png"              //angeben wo das icon liegt
+            iconUrl: "icons/panoramicview.png"
         })
     }).addTo(overlay.adlerblicke);
     //L.marker([blick.lat,blick.lng]).addTo(map);
-    mrk.bindPopup(`Standort ${blick.standort} (${blick.seehoehe}m)`);     //Marker bind pop up, 
+    mrk.bindPopup(`Standort ${blick.standort} (${blick.seehoehe}m)`);
 }
 overlay.adlerblicke.addTo(map);
 
+let drawEtappe = function (nr) {
+    overlay.etappen.clearLayers();
 
-let drawEtappe = function(nr) {
     //console.log(ETAPPEN[nr].track);
     let track = ETAPPEN[nr].track.replace("A", "");
     //console.log(track);
@@ -56,7 +58,7 @@ let drawEtappe = function(nr) {
     let gpx = new L.GPX(`gpx/AdlerwegEtappe${track}.gpx`, {
         async: true,
         marker_options: {
-            startIconUrl: "icons/number_1.png",
+            startIconUrl: `icons/number_${nr}.png`,
             endIconUrl: "icons/finish.png",
             shadowUrl: null,
             iconSize: [32, 37],
@@ -69,29 +71,36 @@ let drawEtappe = function(nr) {
         }
     });
 
-    gpx.on("loaded", function(evt) {
+    gpx.on("loaded", function (evt) {
         map.fitBounds(evt.target.getBounds());
     }).addTo(overlay.etappen);
     overlay.etappen.addTo(map);
 
+    for (const key in ETAPPEN[nr]) {
+        if (ETAPPEN[nr].hasOwnProperty(key)) {
+            const val = ETAPPEN[nr][key];
+            //console.log(`et-${key}`);
+            let elem = document.querySelector(`#et-${key}`);
+            if (elem) {
+                elem.innerHTML = val;
+                //console.log(val);
+            }
+        }
+    }
+
 };
-drawEtappe(10);
+drawEtappe(1);
 
 let pulldown = document.querySelector("#pulldown");
- //console.log(pulldown);
+//console.log(pulldown);
 
- for (let i = 1; i < ETAPPEN.length; i++) {
-     const etappe = ETAPPEN[i];
-     //console.log(etappe);
-     pulldown.innerHTML += `<option value="${i}">${etappe.titel}</option>`;
- }
-
- pulldown.onchange = function(evt) {
+for (let i = 1; i < ETAPPEN.length; i++) {
+    const etappe = ETAPPEN[i];
+    //console.log(etappe);
+    pulldown.innerHTML += `<option value="${i}">${etappe.titel}</option>`;
+}
+pulldown.onchange = function (evt) {
     let nr = evt.target.options[evt.target.options.selectedIndex].value;
     //console.log(nr);
     drawEtappe(nr);
 }
-
-
-
-
