0b8eb6c05e75fd58832f9eb3f33b9b49e13e3ee1
Marco Hilpert <marcohilpert@Marcos-MacBook-Air.local>
2020-05-05 11:20:37 +0200

    etappe pull down erzeugen

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -48,23 +48,44 @@ for (const blick of ADLERBLICKE) {
 overlay.adlerblicke.addTo(map);
 
 
-let gpx = new L.GPX("gpx/AdlerwegEtappe01.gpx", {
-    async: true,
-    marker_options: {
-        startIconUrl: "icons/number_1.png",
-        endIconUrl: "icons/finish.png",
-        shadowUrl: null,
-        iconSize: [32, 37],
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -37]
-    },
-    polyline_options: {
-        color: "black",
-        dashArray: [2, 5]
-    }
-});
+let drawEtappe = function(nr) {
+    //console.log(ETAPPEN[nr].track);
+    let track = ETAPPEN[nr].track.replace("A", "");
+    //console.log(track);
 
-gpx.on("loaded", function (evt) {
-    map.fitBounds(evt.target.getBounds());
-}).addTo(overlay.etappen);
-overlay.etappen.addTo(map);
+    let gpx = new L.GPX(`gpx/AdlerwegEtappe${track}.gpx`, {
+        async: true,
+        marker_options: {
+            startIconUrl: "icons/number_1.png",
+            endIconUrl: "icons/finish.png",
+            shadowUrl: null,
+            iconSize: [32, 37],
+            iconAnchor: [16, 37],
+            popupAnchor: [0, -37]
+        },
+        polyline_options: {
+            color: "black",
+            dashArray: [2, 5]
+        }
+    });
+
+    gpx.on("loaded", function(evt) {
+        map.fitBounds(evt.target.getBounds());
+    }).addTo(overlay.etappen);
+    overlay.etappen.addTo(map);
+
+};
+drawEtappe(10);
+
+let pulldown = document.querySelector("#pulldown");
+ console.log(pulldown);
+
+ for (let i = 1; i < ETAPPEN.length; i++) {
+     const etappe = ETAPPEN[i];
+     console.log(etappe);
+     pulldown.innerHTML += `<option value="${i}">${etappe.titel}</option>`;
+ }
+
+
+
+
