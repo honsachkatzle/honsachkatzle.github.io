dcfc022d553de3fbece520e6d6d72b509f7b8617
Marco Hilpert <marcohilpert@Marcos-MacBook-Air.local>
2020-05-05 15:29:20 +0200

    erstellen einkehr.js first try

---
+++ adlerweg/einkehr.js
@@ -0,0 +1,9 @@
+const EINKEHR = [
+    [ Etappennummer (9), Lamsenjochhütte, 47.38005, 11.60356 ],
+    [ Etappennummer (9), Binsalm, 47.39449, 11.58392  ],
+    [ Etappennummer (11), Engalm, 47.39909 , 11.55850],
+    [ Etappennummer (12), Falkenhütte, 47.39972 , 11.49895 ],
+    [ Etappennummer (13), Einkehrmöglichkeit, Lat, Lng ],
+    [ Etappennummer (14), Einkehrmöglichkeit, Lat, Lng ]
+    // u.s.w.
+];
