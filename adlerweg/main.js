08ecb022c461dd4774f53e965dd942db5e452912
Marco Hilpert <marcohilpert@Marcos-MacBook-Air.local>
2020-05-05 09:15:01 +0200

    new comment

--- adlerweg/main.js
+++ adlerweg/main.js
@@ -13,28 +13,23 @@ let overlay = {
 };
 
 L.control.layers({
-        "BasemapAT.grau": L.tileLayer.provider("BasemapAT.grau"),
-        "BasemapAT": L.tileLayer.provider("BasemapAT"),
-        "BasemapAT.highdpi": L.tileLayer.provider("BasemapAT.highdpi"),
-        "BasemapAT.terrain": startLayer,
-        "BasemapAT.surface": L.tileLayer.provider("BasemapAT.surface"),
-        "BasemapAT.orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
-        "BasemapAT.overlay": L.tileLayer.provider("BasemapAT.overlay"),
-        "BasemapAT.orthofoto+overlay": L.layerGroup([
-            L.tileLayer.provider("BasemapAT.orthofoto"),
-            L.tileLayer.provider("BasemapAT.overlay")
-        ])
-    }, {
-        "Adlerblicke": overlay.adlerblicke
-    }
-
-).addTo(map);
-
-//console.log(ETAPPEN)
+    "BasemapAT.grau": L.tileLayer.provider("BasemapAT.grau"),
+    "BasemapAT": L.tileLayer.provider("BasemapAT"),
+    "BasemapAT.highdpi": L.tileLayer.provider("BasemapAT.highdpi"),
+    "BasemapAT.terrain": startLayer,
+    "BasemapAT.surface": L.tileLayer.provider("BasemapAT.surface"),
+    "BasemapAT.orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
+    "BasemapAT.overlay": L.tileLayer.provider("BasemapAT.overlay"),
+    "BasemapAT.orthofoto+overlay": L.layerGroup([
+        L.tileLayer.provider("BasemapAT.orthofoto"),
+        L.tileLayer.provider("BasemapAT.overlay")
+    ])
+}, {
+    "Adlerblicke": overlay.adlerblicke
+}).addTo(map);
+//console.log(ETAPPEN);
 //console.log(ADLERBLICKE);
 
-
-yy
 for (const blick of ADLERBLICKE) {
     //console.log(blick);
     let mrk = L.marker([blick.lat, blick.lng], {
@@ -45,15 +40,28 @@ for (const blick of ADLERBLICKE) {
             iconUrl: "icons/panoramicview.png"
         })
     }).addTo(overlay.adlerblicke);
-    //L.marker([blick.lat, blick.lng]).addTo(map);
+    //L.marker([blick.lat,blick.lng]).addTo(map);
     mrk.bindPopup(`Standort ${blick.standort} (${blick.seehoehe}m)`);
 }
 overlay.adlerblicke.addTo(map);
 
- let gpx = new L.GPX("gpx/AdlerwegEtappe01.gpx", {
-     async: true
- });
 
- gpx.on("loaded", function(evt) {
-     map.fitBounds(evt.target.getBounds());
- }).addTo(map); 
+let gpx = new L.GPX("gpx/AdlerwegEtappe01.gpx", {
+    async: true,
+    marker_options: {
+        startIconUrl: "icons/number_1.png",
+        endIconUrl: "icons/finish.png",
+        shadowUrl: null,
+        iconSize: [32, 37],
+        iconAnchor: [16, 37],
+        popupAnchor: [0, -37]
+    },
+    polyline_options: {
+        color: "black",
+        dashArray: [2, 5]
+    }
+});
+
+gpx.on("loaded", function (evt) {
+    map.fitBounds(evt.target.getBounds());
+}).addTo(map);
